62e95013f85b9ba0de03d99d1e2fbfaf75de63fa
grigorjevspam-hue <grigorjevspam@gmail.com>
2025-10-26 19:30:39 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,134 +1,87 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TFile, WorkspaceLeaf } from "obsidian";
+import * as dayjs from "dayjs";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface EventItem {
+	date: string;
+	time?: string;
+	file: TFile;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CalendarCollectorPlugin extends Plugin {
+	private events: EventItem[] = [];
 
 	async onload() {
-		await this.loadSettings();
+		console.log("📅 Calendar Collector loaded");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon("calendar", "Открыть календарь", () => {
+			this.openCalendarView();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: "refresh-calendar",
+			name: "Обновить календарь",
+			callback: () => this.collectDates()
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		await this.collectDates();
 	}
 
-	onunload() {
+	async collectDates() {
+		this.events = [];
+		const files = this.app.vault.getMarkdownFiles();
 
+		for (const file of files) {
+			const content = await this.app.vault.read(file);
+
+			// ищем даты вида YYYY-MM-DD или YYYY-MM-DD HH:mm
+			const regex = /\b(\d{4}-\d{2}-\d{2})(?:[ T](\d{2}:\d{2}))?\b/g;
+			let match;
+
+			while ((match = regex.exec(content)) !== null) {
+				this.events.push({
+					date: match[1],
+					time: match[2],
+					file
+				});
+			}
+		}
+
+		console.log("✅ Найдено событий:", this.events.length);
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	openCalendarView() {
+		const eventsByDate = this.groupEventsByDate();
+
+		let html = `<h2>Календарь заметок</h2>`;
+		for (const [date, items] of Object.entries(eventsByDate)) {
+			html += `<h3>${date}</h3><ul>`;
+			for (const e of items) {
+				html += `<li><a href="obsidian://open?vault=${this.app.vault.getName()}&file=${encodeURIComponent(e.file.path)}">${e.file.basename}</a>${e.time ? " – " + e.time : ""}</li>`;
+			}
+			html += "</ul>";
+		}
+
+		const modal = new CalendarModal(this.app, html);
+		modal.open();
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	groupEventsByDate() {
+		const map: Record<string, EventItem[]> = {};
+		for (const e of this.events) {
+			if (!map[e.date]) map[e.date] = [];
+			map[e.date].push(e);
+		}
+		return map;
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class CalendarModal extends import("obsidian").Modal {
+	contentHtml: string;
+	constructor(app: App, html: string) {
 		super(app);
+		this.contentHtml = html;
 	}
-
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		this.contentEl.innerHTML = this.contentHtml;
 	}
 }
